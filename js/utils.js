fe6c32eddda4865b26387bee887e3787896c383a
chihungyu1116 <chihungyu1116@gmail.com>
2016-01-05 08:46:02 -0800

    working on lightbox

---
+++ js/utils.js
@@ -0,0 +1,33 @@
+var Ajax = (function(){
+  var OK = 200;
+
+  function request( method, url ) {
+    return new Promise(function( resolve, reject ) {
+      var req = new XMLHttpRequest();
+      req.open( method, url );
+
+      req.onload = function() {
+        if (req.status === OK) {
+          resolve(req.response);
+        } else {
+          reject(Error(req.statusText));
+        }
+      };
+
+      req.onerror = function() {
+        reject(Error("Network Error"));
+      };
+
+      req.send();
+    });
+  }
+
+  return {
+    get: function( url ){
+      return request( 'GET', url );
+    },
+    post: function( url ) {
+      return request( 'POST', url );
+    }
+  }
+}());
