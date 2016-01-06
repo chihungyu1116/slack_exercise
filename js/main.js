495cfd28e1a33ddb15de7318729036ab734ad7de
chihungyu1116 <chihungyu1116@gmail.com>
2016-01-06 00:39:39 -0800

    working on refactor

--- js/main.js
+++ js/main.js
@@ -1,5 +1,106 @@
 'use strict'
 
+var Template = {
+  renderLightboxPhoto: function( title, imgUrl ) {
+    return (
+      '<div class="thumbnail-caption">' + title + '</div>' +
+      '<img class="lightbox-img respect-ratio-img" src="' + imgUrl + '" ' + 'alt="' + title + '"/>'
+    );
+  },
+  renderPhoto: function() {
+
+  }
+}
+
+// var PhotoView = {
+//   handlePhotoClick: function(event) {
+//     var el = event.target;
+
+//     while(el && el.className !== "thumbnail"){
+//       el = el.parentNode;
+//     }
+
+//     var index = el.id.match(/\d+/)[0];
+
+//     this.currentPhotoIndex = parseInt(index);
+//     this.renderLightbox();
+//   }
+
+// }
+
+function PhotoView( el, photos ) {
+  this.el = document.querySelector( el );
+  this.photos = photos;
+  this.currentIndex = 0;
+}
+
+PhotoView.prototype.getCurrentPhoto = function() {
+  return this.photos[this.currentIndex];
+}
+
+PhotoView.prototype.renderPhoto = function( id, title, imgUrl ) {
+  var el = document.createElement('div')
+
+  el.id = 'thumbnail-' + id
+  el.className = 'thumbnail';
+  el.innerHTML = (
+    '<div class="thumbnail-caption">' + title + '</div>' +
+    '<img class="thumbnail-img respect-ratio-img" src="' + imgUrl + '" ' + 'alt="' + title + '"/>'
+  );
+
+  return el;
+}
+
+PhotoView.prototype.render = function() {
+  var that = this;
+  var container = document.createElement( 'div' );
+
+  container.className = 'clearfix';
+
+  this.photos.forEach(function( photo, index ) {
+    var el = that.renderPhoto( index, photo.title, photo.imgUrl );
+    container.appendChild( el );
+  });
+
+  this.el.innerHTML = '';
+  this.el.appendChild( container );
+}
+
+
+
+function LightboxView( el ) {
+  this.el = document.querySelector( el );
+}
+
+LightboxView.prototype.render = function() {
+  var body = document.querySelector('body');
+  var lightbox = document.createElement('div');
+}
+
+  // var photo = this.photos[this.currentPhotoIndex];
+  var body = document.querySelector('body');
+  var lightbox = document.createElement('div');
+
+  lightbox.classList.add('lightbox');
+
+  lightbox.innerHTML = (
+    '<div class="btn btn-left">Prev</div>' +
+    '<div class="btn btn-right">Next</div>' +
+    '<div id="lightbox-photo"></div>'
+  );
+
+  var overlay = document.createElement('div');
+  overlay.classList.add('overlay');
+  overlay.classList.add('overlay-opened');
+
+  overlay.appendChild(lightbox);
+  body.appendChild(overlay);
+
+  this.renderLightboxPhoto();
+  lightbox.addEventListener('click', this.handleLightboxClick.bind(this));
+
+}
+
 var App = {
   el: document.getElementById( 'photos' ),
   photos: [],
@@ -7,35 +108,45 @@ var App = {
   initialize: function() {
     var that = this;
 
+    this.lightboxView = new LightboxView( '#lightbox' )
+
+
     Flickr.getRecentPhotos().then(function( photos ) {
       that.photos = photos;
-      that.renderPhotos();
+      that.photoView = new PhotoView( '#photos', photos );
+      that.render();
     });
 
     this.el.addEventListener( 'click', that.handlePhotoClick.bind(this) )
   },
-  renderPhoto: function( id, title, imgUrl ) {
-    var el = document.createElement('div')
-    el.className = "thumbnail";
 
-    return (
-      '<div id="thumbnail-' + id + '" class="thumbnail">' +
-        '<div class="thumbnail-caption">' + title + '</div>' +
-        '<img class="thumbnail-img respect-ratio-img" src="' + imgUrl + '" ' + 'alt="' + title + '"/>' +
-      '</div>'
-    )
+  render: function() {
+    this.photoView.render()
   },
-  renderPhotos: function() {
-    var that = this;
 
-    var photos = this.photos.map(function( photo, index ) {
-      return that.renderPhoto( index, photo.title, photo.imgUrl );
-    }).join( '' );
 
-    var html = '<div class="clearfix">' + photos + '<div>';
+  // renderPhoto: function( id, title, imgUrl ) {
+  //   var el = document.createElement('div')
+  //   el.className = "thumbnail";
 
-    this.el.innerHTML = html;
-  },
+  //   return (
+  //     '<div id="thumbnail-' + id + '" class="thumbnail">' +
+  //       '<div class="thumbnail-caption">' + title + '</div>' +
+  //       '<img class="thumbnail-img respect-ratio-img" src="' + imgUrl + '" ' + 'alt="' + title + '"/>' +
+  //     '</div>'
+  //   )
+  // },
+  // renderPhotos: function() {
+  //   var that = this;
+
+  //   var photos = this.photos.map(function( photo, index ) {
+  //     return that.renderPhoto( index, photo.title, photo.imgUrl );
+  //   }).join( '' );
+
+  //   var html = '<div class="clearfix">' + photos + '<div>';
+
+  //   this.el.innerHTML = html;
+  // },
   renderLightbox: function() {
     var photo = this.photos[this.currentPhotoIndex];
     var body = document.querySelector('body');
@@ -46,10 +157,7 @@ var App = {
     lightbox.innerHTML = (
       '<div class="btn btn-left">Prev</div>' +
       '<div class="btn btn-right">Next</div>' +
-      '<div id="lightbox-photo">' +
-        '<div class="thumbnail-caption">' + photo.title + '</div>' +
-        '<img class="lightbox-img respect-ratio-img" src="' + photo.imgUrl + '" ' + 'alt="' + photo.title + '"/>' +
-      '</div>'
+      '<div id="lightbox-photo"></div>'
     );
 
     var overlay = document.createElement('div');
@@ -57,12 +165,16 @@ var App = {
     overlay.classList.add('overlay-opened');
 
     overlay.appendChild(lightbox);
-
     body.appendChild(overlay);
 
+    this.renderLightboxPhoto();
     lightbox.addEventListener('click', this.handleLightboxClick.bind(this));
   },
-
+  renderLightboxPhoto: function() {
+    var photo = this.getCurrentPhoto();
+    var el = document.getElementById('lightbox-photo');
+    el.innerHTML = Template.renderLightboxPhoto( photo.title, photo.imgUrl );
+  },
   handleLightboxClick: function(event) {
     var el = event.target;
 
@@ -81,26 +193,23 @@ var App = {
     }
 
     this.currentPhotoIndex += direction;
-
-    var photo = this.photos[this.currentPhotoIndex];
-    var el = document.getElementById('lightbox-photo');
-
-    el.innerHTML = (
-      '<div class="thumbnail-caption">' + photo.title + '</div>' +
-      '<img class="lightbox-img respect-ratio-img" src="' + photo.imgUrl + '" ' + 'alt="' + photo.title + '"/>'
-    )
+    this.renderLightboxPhoto();
   },
   handlePhotoClick: function(event) {
     var el = event.target;
 
-    while(el && el.className !== "thumbnail"){
+    while(el && !el.classList.contains( "thumbnail" )) {
       el = el.parentNode;
     }
 
     var index = el.id.match(/\d+/)[0];
+    this.photoView.currentIndex = parseInt(index);
 
     this.currentPhotoIndex = parseInt(index);
     this.renderLightbox();
+  },
+  getCurrentPhoto: function() {
+    return this.photos[this.currentPhotoIndex];
   }
 }
 
--- js/utils.js
+++ js/utils.js
@@ -33,3 +33,39 @@ var Ajax = (function(){
     }
   }
 }());
+
+var Flickr = (function(){
+  var apiKey = '69b27790ca5dbdd8d69859ea5793a017';
+  var perPage = 20;
+  var recentPhotoUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&format=json&nojsoncallback=1&api_key=' + apiKey + '&per_page=' + perPage;
+
+  /*
+    photo = {
+      farm: 2,
+      id: "23556008274",
+      isfamily: 0,
+      isfriend: 0,
+      ispublic: 1,
+      owner: "119744273@N02",
+      secret: "533d199fd9",
+      server: "1504",
+      title: ""
+    };
+  */
+  function buildImgObj( photo ) {
+    var imgUrl = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
+    
+    return {
+      title: photo.title,
+      imgUrl: imgUrl
+    };
+  }
+
+  return {
+    getRecentPhotos: function() {
+      return Ajax.get( recentPhotoUrl ).then( JSON.parse ).then(function( response ){
+        return response.photos.photo.map( buildImgObj );
+      });
+    }
+  }
+}());
