a10083f215f22ebc07b5d067edc983ea01a8da19
Chihung Yu <cyu0@walmartlabs.com>
2016-01-05 19:31:20 -0800

    wip

--- js/main.js
+++ js/main.js
@@ -1,89 +1,40 @@
 'use strict'
 
-var Flickr = (function(){
-  var apiKey = '69b27790ca5dbdd8d69859ea5793a017';
-  var perPage = 20;
-  var recentPhotoUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&format=json&nojsoncallback=1&api_key=' + apiKey + '&per_page=' + perPage;
-
-  /*
-    photo = {
-      farm: 2,
-      id: "23556008274",
-      isfamily: 0,
-      isfriend: 0,
-      ispublic: 1,
-      owner: "119744273@N02",
-      secret: "533d199fd9",
-      server: "1504",
-      title: ""
-    };
-  */
-  function buildImgObj( photo ) {
-    var title = photo.title;
-    var imgUrl = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
-    
-    return {
-      title: title,
-      imgUrl: imgUrl
-    };
-  }
-
-  return {
-    getRecentPhotos: function() {
-      return Ajax.get( recentPhotoUrl ).then( JSON.parse ).then(function( response ){
-        return response.photos.photo.map( buildImgObj );
-      });
-    }
-  }
-}());
-
-var Template = {
-  renderPhoto: function( title, imgUrl ) {
-    return [
-      '<div class="thumbnail">',
-        '<img class="thumbnail-img" src="' + imgUrl + '" ' + 'alt="' + title + '"/>',
-        '<div class="thumbnail-caption">' + title + '</div>',
-      '</div>'
-    ].join('');
-  },
-  renderPhotos: function( photos ) {
-    var photoTplArr = this.photos.map(function( photo ) {
-      return Template.renderPhoto( photo.title, photo.imgUrl );
-    });
-
-    return [
-      '<div class="clearfix">',
-        photoTplArr.join(''),
-      '<div>'
-    ]
-  }
-}
-
-
 var App = {
-  el: document.getElementById('photos'),
+  el: document.getElementById( 'photos' ),
   photos: [],
 
-  render: function() {
-    var photosTpl = this.photos.map(function( photo ) {
-      return Template.renderPhoto( photo.title, photo.imgUrl );
-    }).join('');
+  buildPhoto: function( title, imgUrl ) {
+    return (
+      '<div class="thumbnail">' +
+        '<img class="thumbnail-img" src="' + imgUrl + '" ' + 'alt="' + title + '"/>' +
+        '<div class="thumbnail-caption">' + title + '</div>' +
+      '</div>'
+    )
+  },
+  renderPhotos: function() {
+    var that = this;
 
-    var html = [
-      '<div class="clearfix">',
-        photosTpl,
-      '<div>'
-    ].join('');
+    var photos = this.photos.map(function( photo ) {
+      return that.buildPhoto( photo.title, photo.imgUrl );
+    }).join( '' );
+
+    var html = '<div class="clearfix">' + photos + '<div>';
 
     this.el.innerHTML = html;
+  },
+  renderLightbox: function() {
+    console.log('this', this)
   },
   initialize: function() {
     var that = this;
 
     Flickr.getRecentPhotos().then(function( photos ) {
       that.photos = photos;
-      that.render();
+      that.renderPhotos();
     });
+
+    this.el.addEventListener( 'click', that.renderLightbox )
   }
 }
 
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var Ajax = (function(){
   var OK = 200;
 
