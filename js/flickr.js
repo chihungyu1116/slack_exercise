2558abf041ebda29a948b9a4be305722d0716b15
Chihung Yu <cyu0@walmartlabs.com>
2016-01-06 20:50:35 -0800

    done

--- js/flickr.js
+++ js/flickr.js
@@ -1,39 +1,42 @@
-'use strict'
+( function ( window ) {
 
-// Required utils.js
+'use strict';
 
-var Flickr = (function(){
-  var apiKey = '69b27790ca5dbdd8d69859ea5793a017';
-  var perPage = 20;
-  var recentPhotoUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&format=json&nojsoncallback=1&api_key=' + apiKey + '&per_page=' + perPage;
+var API_KEY = '69b27790ca5dbdd8d69859ea5793a017';
+var PER_PAGE = 20;
+var RECENT_PHOTO_API = 'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&format=json&nojsoncallback=1&api_key=' +
+  API_KEY + '&per_page=' + PER_PAGE;
 
-  /*
-    photo = {
-      farm: 2,
-      id: "23556008274",
-      isfamily: 0,
-      isfriend: 0,
-      ispublic: 1,
-      owner: "119744273@N02",
-      secret: "533d199fd9",
-      server: "1504",
-      title: ""
-    };
-  */
-  function buildImgObj( photo ) {
-    var imgUrl = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
-    
-    return {
-      title: photo.title,
-      imgUrl: imgUrl
-    };
-  }
+function buildImgObj( photo ) {
+  // Example
+  // @result = [{
+  //   farm: 2,
+  //   id: "23556008274",
+  //   isfamily: 0,
+  //   isfriend: 0,
+  //   ispublic: 1,
+  //   owner: "119744273@N02",
+  //   secret: "533d199fd9",
+  //   server: "1504",
+  //   title: ""
+  // }, ...]
+  photo.imgUrl = 'https://farm' + photo.farm + '.staticflickr.com/' + 
+    photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
+  
+  return photo;
+}
 
-  return {
-    getRecentPhotos: function() {
-      return Ajax.get( recentPhotoUrl ).then( JSON.parse ).then(function( response ){
+var Flickr = {
+  getRecentPhotos: function() {
+    return $ajax.get( RECENT_PHOTO_API )
+      .then( JSON.parse )
+      .then( function( response ) {
         return response.photos.photo.map( buildImgObj );
-      });
-    }
+      } );
   }
-}());
+};
+  
+window.app = window.app || {};
+window.app.Flickr = Flickr;
+
+} )( window );
